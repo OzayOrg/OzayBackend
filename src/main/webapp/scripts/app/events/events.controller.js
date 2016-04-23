a1f2c5c53e07d3edbc89ec38d7826f9bf910a825
Vikash Kumar <xvikash.kumar@gmail.com>
2016-04-24 04:45:56 +0530

    view for create event

--- src/main/webapp/scripts/app/events/events.controller.js
+++ src/main/webapp/scripts/app/events/events.controller.js
@@ -3,7 +3,8 @@
 angular.module('ozayApp')
     .controller('EventsController', function ($scope, $state) {
         $scope.pageTitle = 'Events';
-        $scope.contentTitle = 'Events';
+        $scope.contentTitle = 'Events | Create';
+        $scope.role = [];
         $scope.example1model = [];
         $scope.events = [{id: 1, label: "David"}, {id: 2, label: "Jhon"}, {id: 3, label: "Danny"}];
     });
